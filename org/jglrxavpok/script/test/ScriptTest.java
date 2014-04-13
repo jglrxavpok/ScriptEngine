cde0517f974ab3c316cfed32b516b75a1e4b8b9f
jglrxavpok <jglrxavpok@gmail.com>
2014-04-13 16:35:08 +0200

    Added MIT license

--- org/jglrxavpok/script/test/ScriptTest.java
+++ org/jglrxavpok/script/test/ScriptTest.java
@@ -2,6 +2,11 @@ package org.jglrxavpok.script.test;
 
 import org.jglrxavpok.script.Script;
 
+/**
+ * No license here, that's just a test file, do whatever you want with it
+ * @author jglrxavpok
+ *
+ */
 public class ScriptTest
 {
 
